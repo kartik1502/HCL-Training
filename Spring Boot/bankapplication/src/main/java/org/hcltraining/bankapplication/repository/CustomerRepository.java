d2d005d48f06c94a0e8aa58970816f1450f9078b
kartik1502 <kartikkulkarni1411@gmail.com>
2022-11-27 22:35:57 +0530

    bankapplication

--- Spring Boot/bankapplication/src/main/java/org/hcltraining/bankapplication/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package org.hcltraining.bankapplication.repository;
-
-import org.hcltraining.bankapplication.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
-}
