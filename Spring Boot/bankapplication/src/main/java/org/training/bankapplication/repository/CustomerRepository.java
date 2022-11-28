1771c2e1167386d4f3e0a896b968c25f8682569f
kartik1502 <kartikkulkarni1411@gmail.com>
2022-11-28 13:43:46 +0530

    commit

--- Spring Boot/bankapplication/src/main/java/org/training/bankapplication/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package org.training.bankapplication.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.training.bankapplication.entity.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
-}
--- Spring Boot/bankapplication/src/main/java/org/training/bankapplication/service/CustomerService.java
+++
@@ -1,9 +0,0 @@
-package org.training.bankapplication.service;
-
-import org.training.bankapplication.entity.Customer;
-
-public interface CustomerService {
-	
-	String addCustomer(Customer customer, int initialDeposit, String accountType);
-
-}
